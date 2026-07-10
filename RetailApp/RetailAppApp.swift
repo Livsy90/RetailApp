a9b1ac3091ea432a1aa41db6b8862a0b6e9cb8f5
Livsy90 <artmir90@gmail.com>
2026-07-10 11:23:57 +0400

    Initial Commit

---
+++ RetailApp/RetailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetailAppApp.swift
+//  RetailApp
+//
+//  Created by Artem Mir on 10.07.26.
+//
+
+import SwiftUI
+
+@main
+struct RetailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
